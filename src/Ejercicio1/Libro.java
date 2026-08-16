93c620e9fc14f42701af615e933cd0629a96836e
Sebastian Tejedor <sebaspolar982@gmail.com>
2026-08-16 13:38:24 -0500

    feat(ejercicio2): Agrego Ejercicio2 y mejoro Ejercicio1

--- src/Ejercicio1/Libro.java
+++ src/Ejercicio1/Libro.java
@@ -4,17 +4,20 @@ public class Libro {
     String autor;
     int NumerodePaginas;
 
+    // Constructor por defecto
     public Libro() {
         this.titulo = "Don Quijote de la Mancha";
         this.autor = "Miguel de Cervantes";
         this.NumerodePaginas = 100;
     }
+    // Constructor con 2 parámetros
     public Libro(String titulo, String autor, int NumerodePaginas) {
         this.titulo = titulo;
         this.autor = autor;
         this.NumerodePaginas = NumerodePaginas;
     }
 
+    // Funcion para mostrar la información del libro
     public void mostrarInformacion() {
         System.out.println("Título: " + titulo);
         System.out.println("Autor: " + autor);
---
+++ src/Ejercicio2/CuentaBancaria.java
@@ -0,0 +1,27 @@
+package Ejercicio2;
+public class CuentaBancaria {
+    String numeroCuenta;
+    double saldo;
+    String tipoCuenta;
+
+    //Constructor por defecto
+    public CuentaBancaria() {
+        this.numeroCuenta = "7508957432";
+        this.saldo = 1000.0;
+        this.tipoCuenta = "Ahorros";
+    }
+
+    //Constructor con 2 parámetros
+    public CuentaBancaria(String numeroCuenta, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = 0.0;
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    //Constructor con 3 parámetros
+    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoCuenta = tipoCuenta;
+    }
+}
